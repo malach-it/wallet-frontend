cf36e0f3035396452a996c8ab0cb69f201470961
Simon <contact@smn.codes>
2025-09-12 16:49:50 +0200

    refactor: fix type

--- src/hocs/UriHandler/resources.ts
+++ src/hocs/UriHandler/resources.ts
@@ -2,7 +2,7 @@ import { Core } from "@wwwallet-private/client-core";
 
 export type StepHandlerID = "pushed_authorization_request" | "authorize" | "presentation" | "presentation_success" | "protocol_error"
 
-export type StepHandlers = Record<StepHandlerID, (this: StepHandlers, config: {}) => void>;
+export type StepHandlers = Record<StepHandlerID, (config: {}) => void>;
 
 export type LocationProtocolResponse = Exclude<Awaited<ReturnType<InstanceType<typeof Core>['location']>>, { protocol: null }>;
 
