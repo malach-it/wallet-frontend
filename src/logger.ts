206947eaebfb521eae64f6e44bc8e80b71eeeacc
Simon <contact@smn.codes>
2025-09-18 16:56:40 +0200

    refactor: add jsonToLog helper

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,45 @@ import { DISPLAY_CONSOLE, LOG_LEVEL } from "./config";
 
 export type LogLevel = "error" | "info" | "warn" | "debug";
 
+/**
+ * Helper function that translates a json array or object into human
+ * readable plain text for logger.
+ */
+export function jsonToLog(json: any): string {
+	let indt = (i: number) => " ".repeat(i);
+
+	const parse = (input: any, indent: number = 4) => {
+		if (input === null || typeof input !== "object") {
+			return String(input).trim();
+		}
+
+		if (Array.isArray(input)) {
+			return input
+				.map(item => `${indt(indent)}- ${parse(item, indent + 4)}`)
+				.join('\n');
+		}
+
+		return Object.entries(input)
+			.map(([key, value]) => {
+				if (Array.isArray(value)) {
+					return `${indt(indent)}${key}:\n` +
+						value
+							.map(item => `${indt(indent + 4)}- ${parse(item, indent + 8)}`)
+							.join('\n');
+				}
+
+				if (value && typeof value === "object") {
+					return `${indt(indent)}${key}:\n${parse(value, indent + 4)}`;
+				}
+
+				return `${indt(indent)}${key}: ${String(value).trim()}`;
+			})
+			.join('\n');
+	};
+
+	return `\n\n${parse(json)}`
+}
+
 export class Logger {
 	level: LogLevel;
 	logLevels: Array<LogLevel> = ["error", "info", "warn", "debug"];
