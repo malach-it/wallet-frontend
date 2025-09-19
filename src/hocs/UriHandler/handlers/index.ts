15f0bef6af04101b417060f203e555029f645c99
Pascal Knoth <pascal@malach.it>
2025-09-19 08:32:43 +0200

    Core authorization code and credential handlers bootstrap

--- src/hocs/UriHandler/handlers/index.ts
+++ src/hocs/UriHandler/handlers/index.ts
@@ -1,5 +1,6 @@
 export * from './authorizeHandler'
 export * from './credentialOfferHandler'
+export * from './credentialRequestHandler'
 export * from './errorHandler'
 export * from './presentationHandler'
 export * from './presentationSuccessHandler'
