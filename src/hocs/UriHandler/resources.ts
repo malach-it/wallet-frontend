81bf14dc94ac5f01208f177de8f683aa12b7ae87
Simon <contact@smn.codes>
2025-09-25 15:14:57 +0200

    refactor: Correct the StepHandlers type

--- src/hocs/UriHandler/resources.ts
+++ src/hocs/UriHandler/resources.ts
@@ -1,9 +1,9 @@
 import { Core } from "@wwwallet-private/client-core";
 
-export type StepHandlerID = "authorization_request" | "authorize" | "presentation" | "presentation_success" | "credential_request" | "protocol_error"
-
-export type StepHandlers = Record<StepHandlerID, (config: {}) => void>;
-
 export type LocationProtocolResponse = Exclude<Awaited<ReturnType<InstanceType<typeof Core>['location']>>, { protocol: null }>;
 
 export type HandlerFactoryResponse = (this: StepHandlers, data: LocationProtocolResponse['data']|{}) => Promise<void>;
+
+export type StepHandlerID = "authorization_request" | "authorize" | "presentation" | "presentation_success" | "credential_request" | "protocol_error"
+
+export type StepHandlers = Record<StepHandlerID, HandlerFactoryResponse>;
