f00f8296b49f434226268fea697ab03f963916ee
Pascal Knoth <pascal@malach.it>
2025-10-16 12:09:12 +0200

    bootstrap global store for application state management

---
+++ src/store/statusSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const statusSlice = createSlice({
+  name: 'status',
+  initialState: {
+		isOnline: false,
+		pwaInstallable: false
+	},
+  reducers: {
+		setOnline: state => { state.isOnline = true },
+		setOffline: state => { state.isOnline = false },
+		setPwaInstallable: state => { state.pwaInstallable = true },
+		setPwaNotInstallable: state => { state.pwaInstallable = false },
+  }
+});
+
+export const {
+	setOnline,
+	setOffline,
+	setPwaInstallable,
+	setPwaNotInstallable,
+} = statusSlice.actions;
+export default statusSlice.reducer;
