f9a796fe62cc5a9e90e4869e72a70181f302cb36
Pascal Knoth <pascal@malach.it>
2025-10-21 12:04:11 +0200

    Bootstrap wwwallet client core

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 export const BACKEND_URL = import.meta.env.VITE_WALLET_BACKEND_URL;
 export const DID_KEY_VERSION: DidKeyVersion = import.meta.env.VITE_DID_KEY_VERSION as DidKeyVersion;
 export const DISPLAY_CONSOLE = import.meta.env.VITE_DISPLAY_CONSOLE;
-
+export const CORE_CONFIGURATION = import.meta.env.VITE_CORE_CONFIGURATION;
 export const MULTI_LANGUAGE_DISPLAY: boolean = import.meta.env.VITE_MULTI_LANGUAGE_DISPLAY ? JSON.parse(import.meta.env.VITE_MULTI_LANGUAGE_DISPLAY) : false;
 export const I18N_WALLET_NAME_OVERRIDE: string | undefined = import.meta.env.VITE_I18N_WALLET_NAME_OVERRIDE;
 export const INACTIVE_LOGOUT_MILLIS = (import.meta.env.VITE_INACTIVE_LOGOUT_SECONDS ? parseInt(import.meta.env.VITE_INACTIVE_LOGOUT_SECONDS, 10) : 60 * 15) * 1000
---
+++ src/lib/services/CoreWrappers/ClientStateStore.ts
@@ -0,0 +1,150 @@
+import { useCallback, useContext, useMemo } from 'react';
+import { logger } from '@/logger';
+import type { ClientState, ClientStateStore } from '@wwwallet-private/client-core'
+import SessionContext from '@/context/SessionContext';
+import { generateRandomIdentifier } from '@/lib/utils/generateRandomIdentifier';
+import pkceChallenge from 'pkce-challenge';
+import { exportJWK, generateKeyPair } from 'jose';
+
+const CLIENT_STATE_KEY = "clientStates"
+
+export function useCoreClientStateStore(): ClientStateStore {
+	const { keystore } = useContext(SessionContext);
+
+	const create = useCallback<ClientStateStore["create"]>(
+		async (issuer, issuer_state) => {
+			const userHandleB64u = keystore.getUserHandleB64u();
+			const state = btoa(
+				JSON.stringify({
+					userHandleB64u: userHandleB64u,
+					id: generateRandomIdentifier(12)
+				})
+			).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+
+				const { code_verifier } = await pkceChallenge()
+
+			const { publicKey, privateKey } = await generateKeyPair("ES256", { extractable: true });
+			const clientState = {
+				issuer,
+				issuer_state,
+				state,
+				code_verifier,
+				dpopKeyPair: {
+					publicKey: await exportJWK(publicKey),
+					privateKey: {
+						alg: "ES256",
+						...await exportJWK(privateKey)
+					},
+				},
+			}
+
+			if (!clientState.issuer_state?.length) {
+				clientState.issuer_state = 'issuer_state'
+			}
+
+			return clientState;
+		}, []);
+
+	const commitChanges = useCallback<ClientStateStore["commitChanges"]>(
+		async (clientState: ClientState): Promise<ClientState> => {
+				const rawClientStates = localStorage.getItem(CLIENT_STATE_KEY) || '[]'
+
+				let found = false;
+				const newClientStates = JSON.parse(rawClientStates).map(
+					(oldClientState) => {
+						if (
+							oldClientState.state === clientState.state &&
+							oldClientState.issuer === clientState.issuer &&
+							oldClientState.issuer_state === clientState.issuer_state
+						) {
+							found = true
+							return clientState
+						}
+						return oldClientState;
+					}
+				)
+
+				if (!found) {
+					newClientStates.push(clientState)
+				}
+
+				localStorage.setItem(CLIENT_STATE_KEY, JSON.stringify(newClientStates.slice(0, 24)))
+
+				return clientState;
+		},
+		[]
+	)
+
+	const fromIssuerState = useCallback<ClientStateStore["fromIssuerState"]>(
+		async (issuer, issuer_state): Promise<ClientState> => {
+				const rawClientStates = localStorage.getItem(CLIENT_STATE_KEY) || '[]'
+
+				const clientStates = JSON.parse(rawClientStates).filter(
+					({ issuer: e, issuer_state: f }) => {
+						return e === issuer && f === (issuer_state || 'issuer_state')
+					}
+				)
+				const clientState = clientStates[clientStates.length - 1]
+
+				if (!clientState) {
+					throw new Error("could not find client state")
+				}
+
+				return clientState;
+		},
+		[]
+	)
+
+	const fromState = useCallback<ClientStateStore["fromState"]>(
+			async (state): Promise<ClientState> => {
+				const rawClientStates = localStorage.getItem(CLIENT_STATE_KEY) || '[]'
+
+				const clientState = JSON.parse(rawClientStates).find(({ state: e }) => e === state)
+
+				if (!clientState) {
+					throw new Error("could not find client state")
+				}
+
+				return clientState;
+			},
+			[]
+	)
+
+	const setCredentialConfigurationIds = useCallback<ClientStateStore["setCredentialConfigurationIds"]>(
+		async (clientState, credentialConfigurationIds): Promise<ClientState> => {
+			const newClientState: ClientState = {
+				...clientState,
+				credential_configuration_ids: credentialConfigurationIds,
+			};
+
+			return newClientState
+		},
+		[]
+	)
+
+	const setIssuerMetadata = useCallback<ClientStateStore["setIssuerMetadata"]>(
+		async (clientState, issuerMetadata): Promise<ClientState> => {
+			const newClientState: ClientState = {
+				...clientState,
+				issuer_metadata: issuerMetadata,
+			};
+
+		return newClientState
+	}, [])
+
+	return useMemo(() => ({
+		create,
+		commitChanges,
+		fromIssuerState,
+		fromState,
+		setCredentialConfigurationIds,
+		setIssuerMetadata,
+	}), [
+		create,
+		commitChanges,
+		fromIssuerState,
+		fromState,
+		setCredentialConfigurationIds,
+		setIssuerMetadata,
+	])
+}
