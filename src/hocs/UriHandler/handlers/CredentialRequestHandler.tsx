96c8a260a909297080b67854f4ab8ea0803065a1
Pascal Knoth <pascal@malach.it>
2025-11-11 10:31:45 +0100

    Cleanup

--- src/hocs/UriHandler/handlers/CredentialRequestHandler.tsx
+++ src/hocs/UriHandler/handlers/CredentialRequestHandler.tsx
@@ -96,6 +96,8 @@ export const CredentialRequestHandler = ({ goToStep, data }) => {
 							}
 						})
 					)
+
+					// TODO commit jwt proof
 					proofs.jwt = proof_jwts
 				}
 
@@ -109,13 +111,9 @@ export const CredentialRequestHandler = ({ goToStep, data }) => {
 							c_nonce
 					).then(({ key_attestation }) => key_attestation)
 
+					// TODO commit attestation proof
 					proofs.attestation = [proof_attestation]
 				}
-
-
-				// TODO commit jwt proof
-				// await api.updatePrivateData(proofsData);
-				// await proofsCommit()
 
 				const credentials = await Promise.all(
 					credential_configuration_ids.map(async (credential_configuration_id: string, index: number) => {
