15c83d2de488d41ff045e46cd2863075f82cff60
Pascal Knoth <pascal@malach.it>
2025-11-19 11:40:31 +0100

    Avoid re-render on parse location errors

--- src/hocs/UriHandler/UriHandler.tsx
+++ src/hocs/UriHandler/UriHandler.tsx
@@ -69,17 +69,18 @@ export const UriHandler = (props: UriHandlerProps) => {
 
 		core.location(window.location).then(presentationRequest => {
 			if (presentationRequest.protocol) {
-				window.history.replaceState(
-					{},
-					'',
-					`${window.location.pathname}?protocol=${presentationRequest.protocol}`,
-				);
+				// window.history.replaceState(
+				// 	{},
+				// 	'',
+				// 	`${window.location.pathname}?protocol=${presentationRequest.protocol}`,
+				// );
 
 				// @ts-expect-error
 				goToStep(presentationRequest.nextStep, presentationRequest.data)
 			}
 		}).catch(err => {
 			if (err instanceof OauthError) {
+				setStep("error")
 				logger.error(t(`errors.${err.error}`), jsonToLog(err));
 				displayError({
 					title: t(`errors.${err.error}`),
